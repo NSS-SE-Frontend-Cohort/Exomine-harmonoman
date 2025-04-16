fcf9de1dd89044dfe2493a3d1da73dd015801204
Mark Bond <markbond615@gmail.com>
2025-04-16 18:33:59 -0500

    Added comment and console.log to Governors.js

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -2,7 +2,9 @@ export const GovernorsDropdown = async () => {
     const governors = await fetch("http://localhost:8088/governors").then(res => res.json());
     console.log("governors: ", governors)
 
+    // Filter active governors from inactive
     const activeGovernors = governors.filter(gov => gov.status);
+    console.log("activeGovernors: ", activeGovernors)
 
 
 
