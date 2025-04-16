b7fe7522928a1d85dc6f194243d8b47fd5190016
Mark Bond <markbond615@gmail.com>
2025-04-16 18:23:21 -0500

    Created fetch call and filtered active governors from database

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
+import { GovernorsDropdown } from "./Governors.js";
+
 const container = document.querySelector("#container");
 
 const render = async () => {
 
+    const governors = GovernorsDropdown();
+
     const composedHTML = 
     `
         <h2>Solar System Mining Marketplace</h2>
-    
+        
     `;
 
     container.innerHTML = composedHTML;
